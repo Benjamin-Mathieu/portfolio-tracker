352b374c0026c85c9b34cab784c8ab63f43a6236
Benjamin Mathieu <ben88200@gmail.com>
2022-03-29 22:29:51 +0200

    Test affichage chart

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,15 +1,40 @@
 <template>
-    <div class="chart">
-        <h3>Vos actifs</h3>
+    <div class="chart-container">
+        <apexchart width="500" type="bar" :options="options" :series="series"></apexchart>
     </div>
 </template>
 
 <script>
+import { reactive, ref } from 'vue'
+import VueApexCharts from 'vue3-apexcharts'
+
 export default {
     name: 'Chart',
+    components: { apexchart: VueApexCharts },
+    setup() {
+        const options = {
+            chart: {
+                id: 'vuechart-example'
+            },
+            xaxis: {
+                categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+            }
+        };
+
+        const series = [{
+            name: 'series-1',
+            data: [30, 40, 45, 50, 49, 60, 70, 91]
+        }];
+
+        return { options, series }
+    }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+.chart-container {
+    display: flex;
+    justify-content: center;
+}
 </style>
